8a5ced1d6d87e877affe67f0ff42bf7f49010dff
Ben Stone <benjamin.h.stone@gmail.com>
2019-10-08 11:26:46 -0400

    add project file

---
+++ Pursuit-Core-iOS-Functions-Lab-Two/Pursuit-Core-iOS-Functions-Lab-Two/functionsProblemsTwo.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+
+// Question One
+
+// Write a function that returns the average of an array of Doubles
+
+func average(of arr: [Double]) -> Double {
+    return -1
+}
+
+// Question Two
+
+// Write a function that takes in an array of Doubles and a Double and returns whether the Double is greater than the average
+
+func value(_ value: Double, isGreaterThanAverageOf arr: [Double]) -> Bool {
+    return false
+}
+
+
+// Question Three
+
+// Given a String as input, rotate all a-z characters by one. This is called ROT-1 encryption.
+// HINT: Use unicode values
+
+func rotOneEncrypt(_ str: String) -> String {
+    return ""
+}
+
+// Question Four
+
+// Decrypt a string that has been encypted by ROT-1 encryption
+
+func rotOneDecrypt(_ str: String) -> String {
+    return ""
+}
+
+// Question Five
+
+// Write a function that returns an array containing the odd numbers from n to m.
+
+func oddNumbers(from m: Int, to n: Int) -> [Int] {
+    return []
+}
+
+// Question Six
+
+// Write a function that returns an array containing the numbers from 1 to n as Strings with the following replacements:
+
+// If the number if a multiple of 3, replace it with "Fizz"
+// If the number is a multiple of 5, replace it with "Buzz"
+// If the number is a multiple of 3 AND 5, replace it with "FizzBuzz"
+
+func fizzBizz(upto n: Int) -> [String] {
+    return []
+}
+
+// Question Seven
+
+// Write a function that returns if an array of Ints is sorted in order from smallest to greatest.
+// BONUS: don't use the .sorted() or .sort() array methods.
+
+
+func isSorted(arr: [Int]) -> Bool {
+    return false
+}
+
+// Question Eight
+
+// Write a function that finds the second smallest Int an an array of Ints
+
+func secondSmallestValue(in arr: [Int]) -> Int {
+    return -1
+}
+
+
